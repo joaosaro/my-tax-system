392105beca5690f665c9d5c41b0353309f2c7832
João Saro <joaovlsaro@gmail.com>
2020-12-27 23:30:09 +0000

    Add Ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./styles/styles.scss";
+import { Button } from "antd";
 
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <Button onClick={() => console.log("helo")}>Hello Button</Button>
+    </div>
+  );
 }
 
 export default App;
