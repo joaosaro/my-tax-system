0df3c6dc29e9888cc5d4d05ebfdf9c9d5a276f61
João <joaovlsaro@gmail.com>
2021-09-12 18:47:15 +0100

    POC - Add TaxSystem context

---
+++ src/app-state/tax-system/tax-system.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, FC, useContext, useState } from "react";
+
+type TaxSystemContextType = {
+  rankCount: number;
+  setRankCount?: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const TaxSystemContext = createContext<TaxSystemContextType>({ rankCount: 5 });
+
+export const TaxSystemProvider: FC = ({ children }) => {
+  const [rankCount, setRankCount] = useState(5);
+
+  return (
+    <TaxSystemContext.Provider value={{ rankCount, setRankCount }}>
+      {children}
+    </TaxSystemContext.Provider>
+  );
+};
+
+export const useTaxSystem = () => useContext(TaxSystemContext);
